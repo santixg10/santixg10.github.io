a663cc4b01c1fdd4ed3f09b83f1ff76809576407
santixg10 <santigallego843@gmail.com>
2025-06-25 12:33:47 -0500

    hv al 96%

--- script.js
+++ script.js
@@ -1,11 +1,23 @@
-function downloadPDF(){
+function downloadPDF() {
+    console.log("Entra");
     const element = document.querySelector('.container-lg');
+
     const opt = {
-        margin: 0,
-        fileman: 'Hoja_de_vida_Santiago_Gallego_Henao.PDF',
-        Image: {type:'jpeg',quality:1},
-        html2canvas: {scale:2, useCors:true},
-        jsPDF: {unit: 'in', format: 'legal', orientation: 'portrait'}
+        margin: [10, 5, 42 , 5],  // [arriba, izquierda, abajo, derecha] en mm
+        filename: 'Hoja_de_Vida_Leandro_Garces.pdf',
+        image: { type: 'jpeg', quality: 0.98 },
+        html2canvas: {
+            scale: 2,
+            useCORS: true,
+            allowTaint: false,
+            scrollY: 0
+        },
+        jsPDF: {
+            unit: 'mm',
+            format: 'a4',
+            orientation: 'portrait'
+        }
     };
-    html2PDF().set(opt).from(element).save();
+
+    html2pdf().set(opt).from(element).save();
 }
